fcc8047331c6de9339e71f1163750e51892cf644
anabramos <anaramosbarretto@gmail.com>
2022-05-10 10:58:14 +0000

    Create view and template for home app

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+    """ A view to return the home page """
+
+    return render(request, 'home/index.html')
