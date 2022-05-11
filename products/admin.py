e31c6fe4b97c95269487e2e2b80b63ffdb0c9096
anabramos <anaramosbarretto@gmail.com>
2022-05-11 11:33:40 +0000

    Add products models to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Habitat, Product
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name')
+
+@admin.register(Habitat)
+class HabitatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'water_need', 'light_need')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name', 'category', 'price', 'size', 'habitat')
