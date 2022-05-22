c37016b2f7cc44032897024694de243052f3d268
anabramos <anaramosbarretto@gmail.com>
2022-05-22 17:18:52 +0000

    create checkout app and models

--- bag/views.py
+++ bag/views.py
@@ -1,27 +1,29 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
 
 # Create your views here.
 
+
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """
     Add product to shopping bag
     """
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-
+        messages.success(request, f'Updated {product.friendly_name} quantity to your bag')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.friendly_name} to your bag')
@@ -35,11 +37,7 @@ def update_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
-
-    if quantity > 0:
-        bag[item_id] = quantity
-    else:
-        bag.pop(item_id)
+    bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -48,8 +46,12 @@ def update_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
-    bag = request.session.get('bag', {})
-    bag.pop(item_id)
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
 
-    request.session['bag'] = bag
-    return redirect(reverse('view_bag'))
+        request.session['bag'] = bag
+        return redirect(reverse('view_bag'))
+
+    except Exception as e:
+        return HttpResponse(status=500)
