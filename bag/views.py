5c00c685cc0da49766bcec5a3c636881e19dfecc
anabramos <anaramosbarretto@gmail.com>
2022-05-22 11:39:35 +0000

    Implement bootstrap toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
 from products.models import Product
 
 # Create your views here.
@@ -12,6 +14,7 @@ def add_to_bag(request, item_id):
     """
     Add product to shopping bag
     """
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -21,6 +24,7 @@ def add_to_bag(request, item_id):
 
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.friendly_name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
