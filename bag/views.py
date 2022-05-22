898f3e6f68c1f5889f5b9d9846685569671b0310
anabramos <anaramosbarretto@gmail.com>
2022-05-22 06:30:35 +0000

    Add update product quantity on bag.html functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse
 from products.models import Product
 
 # Create your views here.
@@ -12,7 +12,6 @@ def add_to_bag(request, item_id):
     """
     Add product to shopping bag
     """
-    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -25,3 +24,18 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def update_bag(request, item_id):
+    """Update quantity of a product to a new amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
