a9714972066beef5cb4720656d510465a53ead6f
anabramos <anaramosbarretto@gmail.com>
2022-05-28 11:50:44 +0000

    Create view to display user profile details on profile template

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
-    """ Display the user's account profile. """
+    """ Display the user's account's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'accounts/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
